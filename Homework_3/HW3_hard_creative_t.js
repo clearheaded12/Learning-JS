d16815626807571c6f2f4ad2ff6831a64f1d6a94
Amir <nodirabody345@gmail.com>
2023-03-15 12:26:40 +0500

    HW3 hard creative task. It was wonderful. I liked this issue.

---
+++ Homework_3/HW3_hard_creative_t.js
@@ -0,0 +1,27 @@
+// Придумайте свой собственный шифр, а также программу, которая будет шифровать и затем дешифровщик к нему
+
+// Nalya solution
+
+let message = `Work smarter, not harder! Be on time`;
+let keyWord = `Java Script!`;
+
+let encryptedMessage = ``;
+for (let i = 0; i < message.length; i++) {
+  for (let e = 0; e < keyWord.length; e++) {
+    if (i == e) {
+      encryptedMessage = encryptedMessage + message[i] + keyWord[e];
+      keyWord += keyWord[e];
+    }
+  }
+}
+console.log(`Keyword = ` + keyWord);
+console.log(`==========`);
+console.log(`Encrypted message = ` + encryptedMessage);
+console.log(`==========`);
+
+let decriptedMessage = ``;
+for (let i = 0; i < encryptedMessage.length; i += 2) {
+  decriptedMessage = decriptedMessage + encryptedMessage[i];
+}
+console.log(`The message = ` + decriptedMessage);
+
